db57a7530a9236957842ca982e11c092e873f898
Akshay Godase <akshayg@bosonmotors.com>
2019-04-18 13:22:47 +0530

    report directory changed

--- Logger.py
+++ Logger.py
@@ -26,7 +26,7 @@ def getLogger(logFilePath='C:\\Results\\'):
 
 def getReportFile():
     loc_time = time.localtime()
-    filename = 'Report_' + str(loc_time.tm_mday) + '_' + str(loc_time.tm_mon) + '_' + str(loc_time.tm_year) + '_' + str(loc_time.tm_hour) + '_' + str(loc_time.tm_min) + '_' + str(loc_time.tm_sec) + '.csv'
+    filename = 'C:\Results\Reports\Report_' + str(loc_time.tm_mday) + '_' + str(loc_time.tm_mon) + '_' + str(loc_time.tm_year) + '_' + str(loc_time.tm_hour) + '_' + str(loc_time.tm_min) + '_' + str(loc_time.tm_sec) + '.csv'
     csv_file = open(filename,'w')
     csv_file.write('TestID,Result\n')
     return csv_file
